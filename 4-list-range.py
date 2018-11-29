78182e9bde1152b154e9bd23af3df827dc717d2d
shamopoo <2570119235@qq.com>
2018-11-29 18:38:34 +0800

    :tada: add some notes

---
+++ 4-list-range.py
@@ -0,0 +1,11 @@
+# /*
+#  * @Author: shamopoo 
+#  * @Date: 2018-11-29 14:41:54 
+#  * @Last Modified by:   shamopoo 
+#  * @Last Modified time: 2018-11-29 14:41:54 
+#  */
+listNum = []
+for value in range(1, 11) :
+  listNum.append(value ** 2)
+
+ print(listNum)
